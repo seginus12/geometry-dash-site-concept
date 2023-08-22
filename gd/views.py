621764abd1e0f19c1c4c7cf59d15d358a9568321
Dmitry Shchcerbakov <dmitri.sherbak@gmail.com>
2023-08-22 14:13:22 +0600

    added redis, working on kubernetes

--- gd/views.py
+++ gd/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Player
+from django.views.decorators.cache import cache_page
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
+from django.conf import settings
+
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
 
-
+@cache_page(CACHE_TTL)
 def index(request):
     return render(request, "gd/mainpage.html")
 
--- gdsite/settings.py
+++ gdsite/settings.py
@@ -129,3 +129,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+        "KEY_PREFIX": "test"
+    }
+}
+
+CACHE_TTL = 60 * 15
